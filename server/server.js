d9be2222248a73a81297c052cac318903e578bdf
mighty-titan <luknoj@gmail.com>
2018-02-12 16:44:40 +0100

    Files upload

---
+++ server/server.js
@@ -0,0 +1,99 @@
+const socket = require('socket.io')();
+const axios = require('axios');
+const Graph = require('graphlib');
+
+var currencyGraph = new Graph.Graph();
+var data = axios.get('https://api.abucoins.com/products/ticker')
+  .then((response) => {
+    (response.data.map((key) => {
+      const splitted = key.product_id.split("-");
+      currencyGraph.setEdge(splitted[0], splitted[1], { value: parseFloat(key.price, 10) });
+      currencyGraph.setEdge(splitted[1], splitted[0], { value: 1 / parseFloat(key.price, 10) });
+    }));
+    console.log("Initial data");
+  })
+setInterval(() => {
+  data = axios.get('https://api.abucoins.com/products/ticker')
+  .then((response) => {
+    (response.data.map((key) => {
+      const splitted = key.product_id.split("-");
+      currencyGraph.setNode(splitted[0]); 
+      currencyGraph.setEdge(splitted[0], splitted[1], { value: parseFloat(key.price, 10) });
+      currencyGraph.setEdge(splitted[1], splitted[0], { value: 1 / parseFloat(key.price, 10) });
+    }));
+    console.log("Refresh data time: " + response.data[0].time);
+    return response.data;
+  });
+}, 60000);
+exchange = (value, from, to) => {
+  if(from == to){
+    return value;
+  } 
+  if((Graph.alg.dijkstra(currencyGraph, from))[to].distance === Infinity){
+    return "Cant find the proper exchange to";
+  }
+  if((Graph.alg.dijkstra(currencyGraph, from))[to].distance == 1){
+    return (value *= currencyGraph.edge(from, to).value);
+  } else {
+    var lastNode = (Graph.alg.dijkstra(currencyGraph, from))[to].predecessor
+    var distance = (Graph.alg.dijkstra(currencyGraph, from))[to].distance;
+    while(distance != 0){
+      value *= currencyGraph.edge(from, lastNode).value;
+      from = lastNode;
+      distance--;
+      lastNode = to;
+    }
+    return value;
+  }
+};
+socket.on('connection', (client) => {
+  console.log("Client connected");
+  var intervalId;
+  client.on('exchangeCurrency', (interval, value, from, to) => {
+    if(currencyGraph.nodes().indexOf(from) === -1 || currencyGraph.nodes().indexOf(to) === -1){
+      client.emit("errorServer", "Please choose a correct currency!");
+      client.disconnect();
+    }
+    if(typeof value != "number") 
+    {
+      client.emit("errorServer", "Please give a number!");
+      client.disconnect();
+    }
+    if(value <= 0) 
+    {
+      client.emit("errorServer", "Please give amouth bigger than 0");
+      client.disconnect();
+    }
+    data
+      .then((response) => {
+        client.emit('currency', {
+          value: exchange(value, from, to),
+          currency: to,
+          date: new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(),
+        });
+      })
+    clearInterval(intervalId);
+    intervalId = setInterval((value, from ,to) => {
+      var date = new Date();
+      data
+      .then((response) => {
+        client.emit('currency', {
+          value: exchange(value, from, to),
+          currency: to,
+          date: date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        client.emit("error", "Please choose a correct currency!");
+      });
+    }, interval, value, from ,to);
+    client.on("disconnect", () => {
+      console.log("Client disconnected");
+      clearInterval(intervalId);
+    });
+  });
+});
+
+socket.listen(8002);
+console.log("Listening on port 8000");
---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import axios from 'axios';
+import './App.css';
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://localhost:8002');
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      exchangedValue: null,
+      value: null,
+      from: "ETH",
+      to: "ETH",
+      currencies: [],
+      data: [],
+      error: null,
+    };
+    socket.on("currency", (output) => this.setState({ exchangedValue: output.value + " " + output.currency, data: [...this.state.data, output] }));
+    socket.on("errorServer", (output) => this.setState({ error: output }));
+  }
+  handleClick(){
+    socket.open();
+    this.setState({ 
+      data: [], 
+      error: null,
+      exchangedValue: null 
+    });
+    socket.emit("exchangeCurrency", 15000, parseFloat(this.state.value), this.state.from, this.state.to, null);
+  }
+  componentDidMount(){
+    this.getCurrencies();
+  }
+
+  getCurrencies = () => {
+    axios.get('https://api.abucoins.com/products/ticker')
+    .then(response => response.data.reduce((memo, key) => {
+      return new Set([...memo, ...key.product_id.split("-")])
+    }, new Set()))
+    .then((result) => {
+      let currencies = Array.from(result);
+      this.setState({ currencies });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  renderCurrencies = () => {
+    if(this.state.currencies === [])
+    {
+      return <p>{this.state.currencies}</p>
+    }
+    else {
+      return this.state.currencies.map((value, index) => {
+        return(
+          <option key={index} value={value}>{value}</option>
+        )
+      })
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-6 input-form">
+              <form action="">
+                <div className="form-group">
+                  <input className="form-control" type="text" pattern="[1-9]*"placeholder="value" onChange={(e) => { this.setState({ value: e.target.value })}} required/>
+                </div>
+                <div className="row">
+                  <div className="form-group col-6">
+                    <label htmlFor="from">From</label>
+                    <select className="form-control" name="from" onChange={(e) => this.setState({ from: e.target.value })} >
+                      {this.renderCurrencies()}
+                    </select>
+                  </div>
+                  <div className="form-group col-6">
+                    <label htmlFor="to">To</label>
+                    <select className="form-control" name="to" onChange={(e) => this.setState({ to: e.target.value })}>
+                      {this.renderCurrencies()}
+                    </select>
+                  </div>
+                </div>
+              </form>
+              <div className="text-center">
+                <button className="btn btn-primary" placeholder="value" onClick={() => this.handleClick()}>Submit</button>
+                {this.state.exchangedValue !== null ? <p className="text-success" > {this.state.exchangedValue} </p>: "" }
+              </div>
+              <div className="text-center">
+                {this.state.error != null ? <p className="text-danger">{this.state.error}</p> : ""}
+              </div>
+              <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+              <Line type="monotone" dataKey="value" stroke="#000000" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              </LineChart>
+            </div> 
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
